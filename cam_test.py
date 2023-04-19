8021b6a5b424ea24a55f3ab3d812bc72c890feba
Pure-Zzzz <73814732+Pure-Zzzz@users.noreply.github.com>
2023-04-19 09:29:46 +0800

    Update cam_test.py

--- cam_test.py
+++ cam_test.py
@@ -1,7 +1,9 @@
 import cv2
 cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,w*2)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,h)
 while(1):
     _, frame = cap.read()
     assert _, "摄像头获取失败"
